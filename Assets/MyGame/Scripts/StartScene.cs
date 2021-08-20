5d877a13f5df526c6687adf58276b27b33e6e21f
张振华 <hellozzhgg@163.com>
2021-08-20 19:18:16 +0800

    开始游戏场景脚本添加

---
+++ Assets/MyGame/Scripts/StartScene.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using FairyGUI;
+using UnityEngine;
+
+public class StartScene : MonoBehaviour
+{
+    private GComponent _mainView;
+
+    private GComponent newContainer;
+    
+    private Controller _viewController;
+
+    private void Awake()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _mainView = this.GetComponent<UIPanel>().ui;
+        _viewController = _mainView.GetController("c1");
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
