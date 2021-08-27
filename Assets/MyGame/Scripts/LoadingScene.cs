cbd7be17b7d5299a31142e370be777269e24342b
ZZH-HP <995718156@qq.com>
2021-08-28 02:13:25 +0800

    no message

---
+++ Assets/MyGame/Scripts/LoadingScene.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using FairyGUI;
+using UnityEngine;
+
+public class LoadingScene : MonoBehaviour
+{
+    private GComponent _mainView;
+
+    private GObject _touchPanel;
+    
+    private Slider
+    
+    private void Awake()
+    {
+            
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _mainView = this.GetComponent<UIPanel>().ui;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
