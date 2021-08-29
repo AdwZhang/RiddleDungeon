0d30e61fcf658a7ee6f0eb96051fdcc287bea790
ZZH-HP <995718156@qq.com>
2021-08-29 08:21:53 +0800

    异步加载完成

--- Assets/MyGame/Scripts/LoadingScene.cs
+++ Assets/MyGame/Scripts/LoadingScene.cs
@@ -9,18 +9,20 @@ public class LoadingScene : MonoBehaviour
     private GComponent _mainView;
 
     private GObject _touchPanel;
-    
-    private Slider
+
+    private GProgressBar _progressBar;
     
     private void Awake()
     {
-            
+        
     }
 
     // Start is called before the first frame update
     void Start()
     {
         _mainView = this.GetComponent<UIPanel>().ui;
+        _touchPanel = _mainView.GetChild("touch_panel");
+        _progressBar = _mainView.GetChild("progress").asProgress;
     }
 
     // Update is called once per frame
@@ -28,4 +30,5 @@ public class LoadingScene : MonoBehaviour
     {
         
     }
+    
 }
--- Assets/MyGame/Scripts/SceneLoadManager.cs
+++ Assets/MyGame/Scripts/SceneLoadManager.cs
@@ -1,31 +1,79 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using FairyGUI;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
 public class SceneLoadManager : MonoBehaviour
 {
-    private static AsyncOperation m_AsyncOperation;
+    private static AsyncOperation m_asyncOperation;
+    private static UnityAction m_finish;
     private static UnityAction<float> m_Progress;
+    private static string m_targetScene;
+    
+    private GComponent _mainView;
+    private GGraph _touchPanel;
+    private GProgressBar _progressBar;
+
+    
     
     // Start is called before the first frame update
     void Start()
     {
+        _mainView = this.GetComponent<UIPanel>().ui;
+        _touchPanel = _mainView.GetChild("touch_panel").asGraph;
+        _touchPanel.onClick.Add(delegate(EventContext context)
+        {
+            m_asyncOperation.allowSceneActivation = true;
+        });
+        _progressBar = _mainView.GetChild("progress").asProgress;
         
+        StartCoroutine("LoadingScene");
+    }
+
+    private IEnumerator LoadingScene()
+    {
+        m_asyncOperation = SceneManager.LoadSceneAsync(m_targetScene); //异步加载1号场景
+        m_asyncOperation.allowSceneActivation = false;                          //不允许场景立即激活//异步进度在 allowSceneActivation= false时，会卡在0.89999的一个值，这里乘以100转整形
+        // 加载完毕后抛出事件
+        m_asyncOperation.completed += delegate(AsyncOperation obj)
+        {
+            m_finish();
+            m_Progress = null;
+            m_finish = null;
+            m_targetScene = null;
+            m_asyncOperation = null;
+        };
+        while (!m_asyncOperation.isDone)
+        {
+            if (m_asyncOperation.progress < 0.9)
+            {
+                m_Progress(m_asyncOperation.progress);
+                _progressBar.value = (int) (m_asyncOperation.progress * 100) + 10;
+            }
+
+            if (_progressBar.value >= 99)
+            {
+                _touchPanel.visible = true;
+                _mainView.GetChild("text").asTextField.visible = true;
+            }
+            
+            yield return null;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (m_AsyncOperation != null)
+        /*if (m_AsyncOperation != null)
         {
             // 抛出加载进度
             if (m_Progress != null)
             {
                 m_Progress(m_AsyncOperation.progress);
             }
-        }
+        }*/
     }
 
     // <summary>
@@ -36,18 +84,10 @@ public class SceneLoadManager : MonoBehaviour
         UnityAction finish)
     {
         // 先加载进入
-        new GameObject("#SceneLoadManager").AddComponent<SceneLoadManager>();
-        m_AsyncOperation = SceneManager.LoadSceneAsync(name, LoadSceneMode.Single);
+        SceneManager.LoadScene("LoadingScene");
         m_Progress = progress;
-        
-        // 加载完毕后抛出事件
-        m_AsyncOperation.completed += delegate(AsyncOperation obj)
-        {
-            finish();
-            m_AsyncOperation = null;
-            m_Progress = null;
-        };
+        m_finish = finish;
+        m_targetScene = name;
     }
-    
-    
+
 }
